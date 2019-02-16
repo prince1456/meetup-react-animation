a83d7d7e492899d1f0ee31a6b9b96d34e63049a4
Ali Alizada <ali.alizada@qileap.com>
2019-02-16 11:04:38 -0800

    fix

---
+++ src/app/containers/Product.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Product extends Component {
+    render(){
+        return (
+            <div>Hello I'm Product</div>
+        )
+    }
+}
+
+export default Product;
